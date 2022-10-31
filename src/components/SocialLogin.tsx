5f20c4a97e8218db68d207f34d55d4aad4f9621f
Jihun Song <songjihun940409@gmail.com>
2022-10-31 19:59:01 +0900

    #4.7 AUTH - Kakao Talk Auth

--- src/components/SocialLogin.tsx
+++ src/components/SocialLogin.tsx
@@ -2,6 +2,12 @@ import { Box, Button, Divider, HStack, Text, VStack } from "@chakra-ui/react";
 import { FaComment, FaGithub } from "react-icons/fa";
 
 export default function SocialLogin() {
+  const kakaoParams = {
+    client_id: "e3da74cb1f90c0b19a8eda08db6781bb",
+    redirect_uri: "http://127.0.0.1:3000/social/kakao",
+    response_type: "code",
+  };
+  const params = new URLSearchParams(kakaoParams).toString();
   return (
     <Box mb={4}>
       <HStack my={8}>
@@ -21,7 +27,13 @@ export default function SocialLogin() {
         >
           Continue With Github
         </Button>
-        <Button w="100%" leftIcon={<FaComment />} colorScheme={"yellow"}>
+        <Button
+          as="a"
+          href={`https://kauth.kakao.com/oauth/authorize?${params}`}
+          w="100%"
+          leftIcon={<FaComment />}
+          colorScheme={"yellow"}
+        >
           Continue With Kakao
         </Button>
       </VStack>
---
+++ src/routes/KakaoConfirm.tsx
@@ -0,0 +1,40 @@
+import { Heading, Spinner, Text, useToast, VStack } from "@chakra-ui/react";
+import { useQueryClient } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { kakaoLogIn } from "../api";
+
+export default function KakaoConfirm() {
+  const { search } = useLocation();
+  const toast = useToast();
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const confirmLogin = async () => {
+    const x = new URLSearchParams(search);
+    const code = x.get("code");
+    if (code) {
+      console.log(code);
+      return;
+      const status = await kakaoLogIn(code);
+      if (status === 200) {
+        toast({
+          status: "success",
+          title: "Welcome!",
+          description: "Happy to have you back!",
+        });
+        queryClient.refetchQueries(["me"]);
+        navigate("/");
+      }
+    }
+  };
+  useEffect(() => {
+    confirmLogin();
+  });
+  return (
+    <VStack justifyContent={"center"} mt={40}>
+      <Heading>Processing log in...</Heading>
+      <Text>Don't go anywhere.</Text>
+      <Spinner size="lg" />
+    </VStack>
+  );
+}
