0445bacd0fd00394a6d1f4127eef1903aeba4b54
Jihun Song <songjihun940409@gmail.com>
2022-11-04 20:31:51 +0900

    #5.6 UPLOAD ROOM - One Time Upload

--- src/routes/UploadPhotos.tsx
+++ src/routes/UploadPhotos.tsx
@@ -7,17 +7,29 @@ import {
   Input,
   VStack,
 } from "@chakra-ui/react";
+import { useMutation } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
 import { useParams } from "react-router-dom";
+import { getUploadURL } from "../api";
 import useHostOnlyPage from "../components/HostOnlyPage";
 import ProtectedPage from "../components/ProtectedPage";
 
-export default function UploadPhotos() {
-  const { register, watch } = useForm();
-  const { roomPk } = useParams();
+interface IForm {
+  file: FileList;
+}
 
+export default function UploadPhotos() {
+  const { register, handleSubmit } = useForm<IForm>();
+  const mutation = useMutation(getUploadURL, {
+    onSuccess: (data: any) => {
+      console.log(data);
+    },
+  });
+  const { roomPk } = useParams();
   useHostOnlyPage();
-  console.log(watch());
+  const onSubmit = (data: any) => {
+    mutation.mutate();
+  };
   return (
     <ProtectedPage>
       <Box
@@ -30,11 +42,16 @@ export default function UploadPhotos() {
       >
         <Container>
           <Heading textAlign={"center"}>Upload a Photo</Heading>
-          <VStack spacing={5} mt={10}>
+          <VStack
+            as="form"
+            onSubmit={handleSubmit(onSubmit)}
+            spacing={5}
+            mt={10}
+          >
             <FormControl>
               <Input {...register("file")} type="file" accept="image/*" />
             </FormControl>
-            <Button w="full" colorScheme={"red"}>
+            <Button type="submit" w="full" colorScheme={"red"}>
               Upload phthos
             </Button>
           </VStack>
